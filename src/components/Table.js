28eecade1d152ac28c6cce188d7f3a0dc4c27b11
shivanshsh4rma <sharmashivansh2211@gmail.com>
2022-08-16 20:54:24 +0530

    minor css changes

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,19 +1,31 @@
+import { useEffect, useState } from "react";
+
 function Table({ data, searchstring, pagenumber}) {
+    const [column, setColumn] = useState("title");
+    const columnfilter = () => {
+        data.sort((a, b) => {
+            console.log(a[column]);
+            if(a[column]-b[column]>0)return 1;
+            if(a[column]-b[column]<0)return -1;
+            return 0;
+          })
+         setTemp(data)
+      }
+    const [temp, setTemp] = useState(data);
     return (
         <div class="table">
             <table id="dtHorizontalExample" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
                 <thead>
-                    <tr>
-                        <th scope="col">Title</th>
-                        <th scope="col">Author</th>
-                        <th scope="col">Subject</th>
-                        <th scope="col">Publish Date</th>
+                    <tr style={{cursor:"pointer"}}>
+                        <th scope="col" onClick={() => {setColumn("title"); columnfilter()}}>Title</th>
+                        <th scope="col" onClick={() => {setColumn("author"); columnfilter()}}>Author</th>
+                        <th scope="col" onClick={() => {setColumn("language"); columnfilter()}}>Language</th>
+                        <th scope="col" onClick={() => {setColumn("year"); columnfilter()}}>Publish Year</th>
                     </tr>
-
                 </thead>
 
                 <tbody id="tableBody">
-                    {data.filter((r, i) => {
+                    {temp.filter((r, i) => {
                         if(JSON.stringify(r).includes(searchstring)) return r; 
                     }).filter((r, i) => {
                         if(i>=pagenumber*10 && i<10*(pagenumber+1)) return r; 
